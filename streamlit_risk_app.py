9d8eedab5e5f9169d3c42f59789b22795caadff7
biggmilkk <scents-thermal9r@icloud.com>
2025-04-20 14:41:58 +0800

    streamlit_risk_app.py

--- streamlit_risk_app.py
+++ streamlit_risk_app.py
@@ -30,7 +30,7 @@ def gpt_extract_risks(scenario_text):
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}],
             temperature=0,
-            top_p=0.0
+            top_p=0.5
         )
 
     content = response.choices[0].message.content
